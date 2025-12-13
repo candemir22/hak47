7244c477cd16e86b2dcdb5ef0b31915a9f003d6e
xarita <128380509+candemir22@users.noreply.github.com>
2025-12-14 03:35:22 +0400

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ async function initMap() {
             position: new google.maps.LatLng({42, 42}),
             collisionBehavior: collisionBehavior,   });
 
+         <gmp-map center="42.773,42.01" zoom="8" map-id="8b37d7206ccf0121d4414bb0">
+  <gmp-advanced-marker position="42.4220656,42.0840897" title="Mountain View, CA"></gmp-advanced-marker>
+  <gmp-advanced-marker position="42.648994,42.3503845" title="Seattle, WA"></gmp-advanced-marker>
+</gmp-map>
+
 
     
     
