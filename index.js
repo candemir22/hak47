f8b93345b7d223c62cb081558b303973509ee8a3
xarita <128380509+candemir22@users.noreply.github.com>
2025-12-14 03:54:10 +0400

    Update index.js

--- index.js
+++ index.js
@@ -13,36 +13,11 @@ async function initMap() {
     const { Map } = (await google.maps.importLibrary('maps'));
     const { AdvancedMarkerElement } = (await google.maps.importLibrary('marker'));
     let markers = [];
-    let collisionBehavior = google.maps.CollisionBehavior.REQUIRED;
-    // @ts-ignore
-    const select = new mdc.select.MDCSelect(document.querySelector('.mdc-select'));
-    select.listen('MDCSelect:change', () => {
-        collisionBehavior = select.value;
-        markers.forEach((marker) => {
-            marker.collisionBehavior = collisionBehavior;
-        });
-    });
-    select.value = collisionBehavior;
-    // Create some markers on the map
-    let locations = [
-        [42.3402, 42.6093],
-        [42.3402, 42.6094],
-        [42.3403, 42.6094],
-        [42.3384, 42.6098],
-        [42.3389, 42.6095],
-        [42.3396, 42.6095],
-
-    ];
-    locations.forEach(([lng, lat]) => {
-        // [START maps_advanced_markers_collision_create_marker]
         const advancedMarker = new AdvancedMarkerElement({
-            position: new google.maps.LatLng({ lat, lng }),
-            collisionBehavior: collisionBehavior,
-        });
-        mapElement.appendChild(advancedMarker);
-        // [END maps_advanced_markers_collision_create_marker]
-        markers.push(advancedMarker);
-    });
+            position: new google.maps.LatLng({42,42})     });
+    
+    
+   
 }
 initMap();
 // [END maps_advanced_markers_collision]
