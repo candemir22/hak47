466f76babcb536ed6afd0aad86fc9af0ff4c33f4
xarita <128380509+candemir22@users.noreply.github.com>
2025-12-14 03:42:43 +0400

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,9 @@ async function initMap() {
     const { Map } = (await google.maps.importLibrary('maps'));
     const { AdvancedMarkerElement } = (await google.maps.importLibrary('marker'));
     let markers = [];
-    gmp-map center="43,42.01" zoom="18" map-id="8b37d7206ccf0121d4414bb0";
-    
+   
         const advancedMarker = new AdvancedMarkerElement({ position="42.4220656,42.0840897"  });
-
-
-
-
-    
-    
+   
 
 }
 initMap();
