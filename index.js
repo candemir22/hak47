1583a4fb559a27315d9c42628d1a7a249c26ca39
xarita <128380509+candemir22@users.noreply.github.com>
2025-12-14 04:13:33 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-"use strict";
-/**
- * @license
- * Copyright 2019 Google LLC. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
-// eslint-disable no-undef
-// [START maps_advanced_markers_collision]
+
 const mapElement = document.querySelector('gmp-map');
-// Initialize and add the map
+
 async function initMap() {
-    // Request needed libraries.
+
     const { Map } = (await google.maps.importLibrary('maps'));
     const { AdvancedMarkerElement } = (await google.maps.importLibrary('marker'));
     let markers = [];
@@ -18,17 +11,10 @@ async function initMap() {
 
     
 
-function kazibol() {
-  if (marker.getAnimation() !== null) {
-    marker.setAnimation(null);
-  } else {
-    	window.speechSynthesis.speak(new SpeechSynthesisUtterance('kazı bölgesi '));
-    marker.setAnimation(google.maps.Animation.BOUNCE);
-  
-  }}
+
 
 function cikis() {
-     marker.setAnimation(null);
+
      	window.speechSynthesis.speak(new SpeechSynthesisUtterance('bu bölgenin proje kodu 128.60'));  
 }
             
